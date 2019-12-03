d1807bcc22b62bdd78d7f2c078f491c387164442
Ruben Branco <ruben.branco@outlook.pt>
2019-12-03 19:03:07 +0000

    Giving more memory to neo4j

--- docker/neo4j/start-container.sh
+++ docker/neo4j/start-container.sh
@@ -39,4 +39,6 @@ docker run \
     --publish=7474:7474 \
     --publish=7687:7687 \
     --volume=$PWD/volumes/neo4j/data:/data \
+    --env=NEO4J_dbms_memory_heap_max__size=4G \
+    $extra_options \
     neo4j:3.5
